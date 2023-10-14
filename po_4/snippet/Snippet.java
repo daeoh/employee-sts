358911edd86e9a54a0b261d3aa350857dc3cad8e
daeoh <82095432+daeoh@users.noreply.github.com>
2023-10-14 15:16:20 +0900

    [TEST] 커밋!!

---
+++ po_4/snippet/Snippet.java
@@ -0,0 +1,8 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		C:\Users\김민재\git\employee-sts\po_4
+	}
+}
+
--- po_4/src/main/resources/static/script/main.js
+++
@@ -1,3 +0,0 @@
-function join()   {window.location.href = "/project/join";}
-function login()  {window.location.href = "/project/login";}
-function logout() {window.location.href = "/project/login";}
